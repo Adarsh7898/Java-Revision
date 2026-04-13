bc974100fa66403f75864c561e46995b2fbcce1b
Adarsh7898 <adarshgoyal220890@acropolis.in>
2026-04-13 20:53:08 +0530

    Add programs upto throw vs throws

---
+++ src/ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,20 @@
+
+class A{
+    int display(){
+        int x=10;
+        try {
+
+            return x;
+        }
+
+        finally {
+//            return 50;
+        }
+    }
+}
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        System.out.println(new A().display());
+    }
+}
